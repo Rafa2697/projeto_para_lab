826341f1f59f4ec70eddb6b0d7a61fa4c30d2fb8
Rafael <Rafael.mattos468@gmail.com>
2023-05-22 21:25:03 -0300

    update

--- functions.js
+++ functions.js
@@ -1,9 +1,10 @@
 
 
 $(function(){
-    $('.box2').css('display','none')
+    $('.box2').css('display','flex')
     $('.img1').css('width','60%')
     $('.img2').css('width','60%')
+    $('.img3').css('width','75%')
     
   $('.box1').click(function(){
     $('.box2').slideToggle().css('display','flex').css('justify-content' ,'space-around')
