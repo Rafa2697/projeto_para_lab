e3cb45fa3c3401c9a8fd9e3c8ba2beb8f6a6027c
Rafael <Rafael.mattos468@gmail.com>
2023-10-18 17:18:50 -0300

    criando função
    essa função deixa a div em block e none ao clicar

--- functions.js
+++ functions.js
@@ -10,3 +10,32 @@ $(function(){
     $('.box2').slideToggle().css('display','flex').css('justify-content' ,'space-around')
   })
 })
+
+
+function mais(){
+  let x = document.getElementById('span');
+  let y = document.getElementById('demo');
+
+  x.addEventListener('click', function(){
+    if(y.style.display === 'block'){
+      y.style.display = 'none'
+    }else{
+      y.style.display = 'block'
+    }
+  });
+
+  x.addEventListener('mouseenter', mouseEnter);
+  x.addEventListener('mouseout', mouseOut);
+
+}
+
+//função para mudar a cor para vermelho do span
+function mouseEnter(){
+  document.getElementById('span').style.color = 'red';
+}
+
+//função para mudar voltar a cor para darkslategray
+function mouseOut(){
+  document.getElementById('span').style.color = 'darkslategray';
+}
+mais()
